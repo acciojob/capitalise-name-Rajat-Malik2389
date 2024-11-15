ecc05f741ac2a5e14851c9a211d9191f6b6298f8
Rajat Malik <133639198+Rajat-Malik2389@users.noreply.github.com>
2024-11-15 14:55:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let inputName = document.getElementById("name")
+let output = document.getElementById("output")
+inputName.addEventListener("blur", (e) => {
+    output.innerHTML = e.target.value.toUpperCase()
+})
+
+
+
