29e06a02e7216a7c79a72da1c734171596c25e84
Rajat Malik <133639198+Rajat-Malik2389@users.noreply.github.com>
2024-11-15 15:01:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //your JS code here. If required.
-let inputName = document.getElementById("name")
-let output = document.getElementById("output")
-inputName.addEventListener("blur", (e) => {
-    output.innerHTML = e.target.value.toUpperCase()
+let ip = document.getElementById("fname")
+ip.addEventListener("blur", (e) => {
+    
+	let temp = ip.value.toUpperCase()
 })
 
 
